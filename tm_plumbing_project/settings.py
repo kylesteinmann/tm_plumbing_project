5226de1fae7873c0207b64df811c1be1d9a66303
kyle Steinmann <kylesteinmann@yahoo.com>
2025-04-05 21:25:25 -0500

    allowed host

--- tm_plumbing_project/settings.py
+++ tm_plumbing_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'django-insecure-j9^9p=+tsg*c(g6f*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os .environ.get('ALLOWED_HOSTS', ['localhost'])
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
